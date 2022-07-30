7e98d97f50e820a33319a773a56a968a1d940b11
Hamza EL Yousfi <visioncomputer200@gmail.com>
2022-07-30 14:51:36 +0200

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -6,22 +6,84 @@ h(x) = thetaT*x
 l(theta) = 1/2m * sum_over_m(thetaT * x- y)²
 """
 
+def transpose(L):
+    n = len(L)
+    m = len(L[0])
+    L_transpose = []
+    for j in range(m):
+        temp = []
+        for i in range(n):
+            temp.append(L[i][j])
+        L_transpose.append(temp)
+    return L_transpose
 
-X = np.array([1, 2, 4, 5, 6 ,8])
-y = np.array([2.5, 4.5, 7, 10.4, 13, 15])
 
-theta = np.array([4, 2])
+def matrixMul(M, L):
 
-print(np.matmul(theta.T, np.array([[1, 1, 1, 1, 1, 1], X])))
+    n1, m1 = len(M), len(M[0])
+    n2, m2 = len(L), len(L[0])
+
+    if m1 == n2:
+        P = []
+        for i in range(n1):
+            line = []
+            for j in range(m2):
+                Cij = 0
+                for k in range(m1):
+                    Cij += M[i][k] * L[k][j]
+                line.append(Cij)
+            P.append(line)
+        return P
+
+def matrixMulalpha(alpha, L):
+    n, m = len(L), len(L[0])
+    for i in range(n):
+        for j in range(m):
+            L[i][j] = alpha * L[i][j]
+    return L
+
+def diff(a, b):
+    n = len(a)
+    temp = []
+    for i in range(n):
+        temp.append(a[i] - b[i])
+    return [temp]
+
+def diff2(a, b):
+    n = len(a)
+    m = len(a[0])
+    d = []
+    for i in range(n):
+        temp = []
+        for j in range(m):
+            temp.append(a[i][j] - b[i][j])
+        d.append(temp)
+    return d
+
+
+# Create the data 
+X = [ [1, 1, 1, 1, 1, 1], 
+      [1, 2.5, 4, 5, 6, 7.5]]
+y = [2.5, 4.5, 7, 10.4, 13, 15]
+
+theta = [ [4], 
+          [2] ]
+
+print(X)
+print(matrixMul(transpose(theta), X)[0])
+
 
 
 def hypothesis(theta, X):
     # h(x) = theta.T * x
-    X_new = np.array([[1, 1, 1, 1, 1, 1], X])
-    return np.matmul(theta, X_new)
+    return matrixMul(transpose(theta), X)[0]
 
 def loss(y_pred, y):
-    return np.sum((y_pred - y)**2)
+    n = len(y_pred)
+    s = 0
+    for i in range(n):
+        s += (y_pred[i] - y[i])**2
+    return s
 
 alpha = 0.01
 
@@ -31,11 +93,9 @@ y_pred = hypothesis(theta, X)
 l = loss(y_pred, y)
 for j in range(1000):
     y_pred = hypothesis(theta, X)
-    theta = theta - alpha * np.matmul((hypothesis(theta, X) - y), np.array([[1, 1, 1, 1, 1, 1], X]).T)
+    print(diff(y_pred, y))
+    theta = diff2(theta, transpose(matrixMulalpha(alpha, matrixMul(diff(hypothesis(theta, X), y), transpose(X)))))
     print(theta)
-
-plt.plot(np.arange(1, 10),hypothesis(theta, np.arange(1, 10)))
-plt.show()
 
 
 
